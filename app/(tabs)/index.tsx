9415528c93e956f70d0eb79e7badbf60390885e5
zeankun.dev <zeanfender11@gmail.com>
2025-03-24 04:57:29 +0700

    now mapping it out for us...

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,31 +9,57 @@ import Colors from '@/constants/Colors';
 import {LinearGradient} from 'react-native-linear-gradient';
 import TextField from '@/components/TextField';
 import AISettings from '@/components/config/AISettings';
+import { useEffect, useState } from 'react';
+
+interface Contact {
+  profilePicture: string;
+  tag: string;
+  name: string;
+}
 
 export default function TabOneScreen() {
+  const [contacts, setContacts] = useState<Contact[] | null>(null);
+  const PROD_JSON_URL = 'https://raw.githubusercontent.com/zeankundev/cdn/refs/heads/main/genai.json';
+  useEffect(() => {
+    const loadContacts = async () => {
+      try {
+        const response = await fetch(PROD_JSON_URL);
+        if (!response.ok) throw new Error('Network response was not ok');
+        const data = await response.json();
+        console.log(data)
+        setContacts(data)
+      } catch (error) {
+        console.error('💥 Error loading contacts:', error);
+      }
+    }
+    loadContacts();
+  }, [contacts])
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Home</Text>
       <View>
         <TextField placeholder='Search for anything (broken atm)'></TextField>
       </View>
-      <TouchableOpacity 
-        style={styles.contactInfo} 
-        onPress={() => router.push({ pathname: "/modal", params: { generativeAITag: AISettings.tag } })}
-      >
-        <View style={styles.contactInfo}>
-          <Image 
-            source={{ uri: AISettings.profilePicture }}
-            width={52}
-            height={52}
-            style={{ borderRadius: 100 }}
-          />
-          <View style={styles.nestedContactInfo}>
-            <Text style={styles.title}>{AISettings.name}</Text>
-            <Text style={styles.subtitle}>Start chatting now...</Text>
+      {contacts?.map((contact, index) => (
+        <TouchableOpacity 
+          style={styles.contactInfo} 
+          onPress={() => router.push({ pathname: "/modal", params: { generativeAITag: contact.tag } })}
+          key={index}
+        >
+          <View style={styles.contactInfo}>
+            <Image 
+              source={{ uri: contact.profilePicture }}
+              width={52}
+              height={52}
+              style={{ borderRadius: 100 }}
+            />
+            <View style={styles.nestedContactInfo}>
+              <Text style={styles.title}>{contact.name}</Text>
+              <Text style={styles.subtitle}>Start chatting now...</Text>
+            </View>
           </View>
-        </View>
-      </TouchableOpacity>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 }
