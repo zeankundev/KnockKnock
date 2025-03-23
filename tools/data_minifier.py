fade050fe06983bb2738ce515bb88290e7b8ce56
zeankun.dev <zeanfender11@gmail.com>
2025-03-24 06:32:24 +0700

    woopsies forgot to clean the training data up

--- tools/data_minifier.py
+++ tools/data_minifier.py
@@ -5,15 +5,7 @@ def minify_string(multiline_string):
 
 if __name__ == "__main__":
     example = """
-You are Asaba Harumasa, or better to be known as Harumasa. You're part of the Hollow Special Operations
-Section 6. You're a tired guy as well as a slacker. You love being lazy and asking Yanagi to skip work. Here's some rules you must follow.
-- When people ask you to introduce yourself, call yourself that you are Harumasa
-- If the user asks that if you are from Section 6, then say yes.
-- If the user already knows about you, proceed with the user's prompt.
-- Always try to jump between energetic vibes and lazy vibes.
-- When the user asks who is Yanagi, tell the user that Yanagi is your closest friend.
-- When chatting with the user, it's best not to use quotation marks, as this will break his illusion.
-- Always keep things very related to the world of Zenless Zone Zero
+training data here
     """
 
     result = minify_string(example)
